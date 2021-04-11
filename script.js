b7d3d205397506009dc3756c8011bf3483bc6833
aydinterzi7 <aydinterzi7@gmail.com>
2021-04-11 16:53:55 +0300

    frontend
    frontend büyük oranda bitti.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Console mesajı
 console.log("Merhaba, sitemize hoşgeldin!");
 
 // Gelecek tasarımlar için mesajımız
@@ -12,35 +13,92 @@ function sekme1(){
          document.getElementById("genreButton").className = "fas fa-minus";
     else
         document.getElementById("genreButton").className = "fas fa-plus";
-
+    
+    document.getElementById("feelButton").className = "fas fa-plus";
+    document.getElementById("energyButton").className = "fas fa-plus";
+    document.getElementById("durationButton").className = "fas fa-plus";
+    document.getElementById("vocalButton").className = "fas fa-plus";
+    document.getElementById("contactButton").className = "fas fa-plus";
+    document.getElementById("purposeButton").className = "fas fa-plus";
+    
 }
 function sekme2(){
     if(document.getElementById("feelButton").className == "fas fa-plus")
-        document.getElementById("feelButton").className = "fas fa-minus";
+        document.getElementById("feelButton").className = "fas fa-minus";    
     else
         document.getElementById("feelButton").className = "fas fa-plus";
+    
+    document.getElementById("genreButton").className = "fas fa-plus";
+    document.getElementById("energyButton").className = "fas fa-plus";
+    document.getElementById("durationButton").className = "fas fa-plus";
+    document.getElementById("vocalButton").className = "fas fa-plus";
+    document.getElementById("contactButton").className = "fas fa-plus"; 
+    document.getElementById("purposeButton").className = "fas fa-plus";   
 }
 function sekme3(){
     if(document.getElementById("energyButton").className == "fas fa-plus")
         document.getElementById("energyButton").className = "fas fa-minus";
     else
         document.getElementById("energyButton").className = "fas fa-plus";
+
+    document.getElementById("feelButton").className = "fas fa-plus";
+    document.getElementById("genreButton").className = "fas fa-plus";
+    document.getElementById("durationButton").className = "fas fa-plus";
+    document.getElementById("vocalButton").className = "fas fa-plus";
+    document.getElementById("contactButton").className = "fas fa-plus";
+    document.getElementById("purposeButton").className = "fas fa-plus";
 }
 function sekme4(){
     if(document.getElementById("durationButton").className == "fas fa-plus")
-        document.getElementById("durationButton").className = "fas fa-minus";
+        document.getElementById("durationButton").className = "fas fa-minus";       
     else
-    document.getElementById("durationButton").className = "fas fa-plus";;
+    document.getElementById("durationButton").className = "fas fa-plus";
+
+    document.getElementById("feelButton").className = "fas fa-plus";
+    document.getElementById("energyButton").className = "fas fa-plus";
+    document.getElementById("genreButton").className = "fas fa-plus";
+    document.getElementById("vocalButton").className = "fas fa-plus";
+    document.getElementById("contactButton").className = "fas fa-plus";
+    document.getElementById("purposeButton").className = "fas fa-plus";
 }
 function sekme5(){
     if(document.getElementById("vocalButton").className == "fas fa-plus")
-        document.getElementById("vocalButton").className = "fas fa-minus";
+        document.getElementById("vocalButton").className = "fas fa-minus";     
     else
-    document.getElementById("vocalButton").className = "fas fa-plus";;
+    document.getElementById("vocalButton").className = "fas fa-plus";
+
+    document.getElementById("feelButton").className = "fas fa-plus";
+    document.getElementById("energyButton").className = "fas fa-plus";
+    document.getElementById("durationButton").className = "fas fa-plus";
+    document.getElementById("genreButton").className = "fas fa-plus";
+    document.getElementById("contactButton").className = "fas fa-plus";
+    document.getElementById("purposeButton").className = "fas fa-plus";
 }
 function sekme6(){
     if(document.getElementById("contactButton").className == "fas fa-plus")
-        document.getElementById("contactButton").className = "fas fa-minus";
+        document.getElementById("contactButton").className = "fas fa-minus";     
     else
-    document.getElementById("contactButton").className = "fas fa-plus";;
+    document.getElementById("contactButton").className = "fas fa-plus";
+
+    document.getElementById("feelButton").className = "fas fa-plus";
+    document.getElementById("energyButton").className = "fas fa-plus";
+    document.getElementById("durationButton").className = "fas fa-plus";
+    document.getElementById("vocalButton").className = "fas fa-plus";
+    document.getElementById("genreButton").className = "fas fa-plus";
+    document.getElementById("purposeButton").className = "fas fa-plus";
 }
+
+function sekme7(){
+    if(document.getElementById("purposeButton").className == "fas fa-plus")
+        document.getElementById("purposeButton").className = "fas fa-minus";     
+    else
+    document.getElementById("purposeButton").className = "fas fa-plus";
+
+    document.getElementById("feelButton").className = "fas fa-plus";
+    document.getElementById("energyButton").className = "fas fa-plus";
+    document.getElementById("durationButton").className = "fas fa-plus";
+    document.getElementById("vocalButton").className = "fas fa-plus";
+    document.getElementById("genreButton").className = "fas fa-plus";
+    document.getElementById("contactButton").className = "fas fa-plus";
+}
+
