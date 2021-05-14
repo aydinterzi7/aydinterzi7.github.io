b842946ae5cfffc78788b2f198f3fea043052d5a
aydinterzi <aydinterzi7@gmail.com>
2021-05-14 12:02:28 +0300

    left-panel-changed

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 console.log("Merhaba, sitemize hoşgeldin!");
 
 // Gelecek tasarımlar için mesajımız
-function uyarı()
-{
+function uyarı() {
     alert("Yakında hizmetinizde!");
+}
+const button = document.querySelector("#mainButton");
+button.addEventListener("click", isCheck);
+
+function isCheck() {
+    const isChecked = document.querySelectorAll("#flexCheckDefault");
+    isChecked.forEach(element => {
+        if (element.checked === true)
+            if ( element.nextElementSibling.textContent.trim()==="HAPPY") {
+                console.log("happy");
+            }
+            else{
+                console.log( element.nextElementSibling.textContent);
+            }
+    });
 }
